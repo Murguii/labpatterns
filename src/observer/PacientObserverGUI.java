8887005470fda2e372803eaf36ec4332780e2efb
Murguii <muurguii10@gmail.com>
2024-10-26 09:39:30 +0200

    aaa

--- src/observer/PacientObserverGUI.java
+++ src/observer/PacientObserverGUI.java
@@ -12,7 +12,7 @@ import domain.Symptom;
 
 import javax.swing.JLabel;
 
-public class PacientObserverGUI extends JFrame{
+public class PacientObserverGUI extends JFrame implements Observer{
 
 	private JPanel contentPane;
 	private final JLabel symptomLabel = new JLabel("");
@@ -20,7 +20,7 @@ public class PacientObserverGUI extends JFrame{
 	/**
 	 * Create the frame.
 	 */
-	public PacientObserverGUI() {
+	public PacientObserverGUI(Observable obs) {
 		setTitle("Pacient symptoms");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(650, 100, 200, 300);
@@ -31,7 +31,26 @@ public class PacientObserverGUI extends JFrame{
 		symptomLabel.setBounds(19, 38, 389, 199);
 		contentPane.add(symptomLabel);
 		symptomLabel.setText("Still no symptoms");
+		obs.addObservers(this);
 		this.setVisible(true);
 	}
+
+	
+	public void update(Observable o, Object arg) {
+		Covid19Pacient p = (Covid19Pacient) o;
+		String s = "<html> Pacient: <b>"+p.getName()+"</b> <br>";
+		s = s + "Covid impact: <b>"+p.getName()+"</b> <br>";
+		s = s + " __________________ <br> Symptoms: <br>";
+		Iterator <Symptom> i = p.getSymptoms();
+		Symptom p2;
+		while(i.hasNext()) {
+			p2 = i.next();
+			s = s + " - " + p2.toString()+ ", " + p.getWeight(p2) + "<br>";
+		}
+		s = s + "</html>";
+		symptomLabel.setText(s);
+			
+	}
+	
 
 }
